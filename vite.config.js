ea382768eb2351f804ca29c1ca54ea9e89659bf5
aurebidart <aure.bidart@gmail.com>
2026-03-24 14:17:25 -0300

    Publish on pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(() => {
+  return {
+    base: process.env.VITE_BASE || '/',
+    plugins: [vue()],
+  }
 })
